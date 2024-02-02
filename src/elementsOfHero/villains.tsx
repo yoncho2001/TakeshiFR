3ca72591f7553bfbb3cc502c663edcc6c216fed6
Yoncho <yoncho.yonchrev@p3-group.com>
2024-02-02 12:03:04 +0200

    try

---
+++ src/elementsOfHero/villains.tsx
@@ -0,0 +1,12 @@
+const villains = new Map<string, Potion>([
+    ["HealthPotion", {
+        name: "HealtPotion",
+        affectingField: "HealthPoints",
+        affectingValue: 50
+    }],
+    ["ManaPotion", {
+        name: "ManaPotion",
+        affectingField: "Mana",
+        affectingValue: 50
+    }],
+]);
